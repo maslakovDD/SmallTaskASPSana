d03a9738d68345fba531bd17951b8f64edd6c2f0
maslakovDD <ipz212_mdd@student.ztu.edu.ua>
2023-03-23 13:53:58 +0200

    small task

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SmallTask.Models;
+using System.Diagnostics;
+using System.Text.Encodings.Web;
+
+namespace SmallTask.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Welcome(string name)
+        {
+            ViewData["Name"] = name;
+            return View();
+        }
+    }
+}
